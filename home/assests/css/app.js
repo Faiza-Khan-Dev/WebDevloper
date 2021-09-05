d9ff0df12a6e532b8920dfc11093abfcc44db4cf
Faiza-Khan-Dev <faizaazizkhan10@gmail.com>
2021-09-05 20:08:41 +0500

    js

---
+++ home/assests/css/app.js
@@ -0,0 +1,5 @@
+var Name = prompt("You See The Magic To Enter Your Name");
+
+console.log("Nice To Meet You "+ Name +" .")
+
+alert("Please Right Click To Inspect Than Console Go To Usear Message To See This Magic")
